4fa27aefdc81a518276d5cea83b326354b36352e
Takashi Toyoshima <toyoshim@gmail.com>
2015-04-05 23:48:45 +0900

    with midiInOpen()

--- MIDIConceptTest.cpp
+++ MIDIConceptTest.cpp
@@ -57,8 +57,8 @@ struct MidiProduct {
 };
 
 struct MidiDevice : MidiProduct {
-	MidiDevice(const MIDIINCAPS2& caps, int index) : MidiProduct(caps), mIndex(index) {}
-	MidiDevice(const MidiProduct& product, int index) : MidiProduct(product), mIndex(index) {}
+	MidiDevice(const MIDIINCAPS2& caps, int index) : MidiProduct(caps), mIndex(index), mHMidi(NULL) {}
+	MidiDevice(const MidiProduct& product, int index) : MidiProduct(product), mIndex(index), mHMidi(NULL) {}
 
 	std::string getDisplayName() const override {
 		std::stringstream ss;
@@ -71,6 +71,7 @@ struct MidiDevice : MidiProduct {
 	}
 
 	int mIndex;
+	HMIDIIN mHMidi;
 };
 
 bool operator<(const MidiProduct& lhs, const MidiProduct& rhs) {
@@ -92,12 +93,14 @@ bool operator<(const MidiDevice& lhs, const MidiDevice& rhs) {
 	return lhs.mIndex < rhs.mIndex;
 };
 
-std::map<MidiProduct, std::set<int>> deviceMap;
-std::list<MidiDevice> deviceList;
+std::map<MidiProduct, std::set<int>> productMap;
+std::map<HMIDIIN, MidiDevice> handleMap;
+std::set<HMIDIIN> closingHandleSet;
+std::set<MidiDevice> deviceSet;
 
 int NextDeviceIndex(const MidiProduct& device) {
-	auto map = deviceMap.find(device);
-	if (map == deviceMap.end())
+	auto map = productMap.find(device);
+	if (map == productMap.end())
 		return 0;
 	for (int id = 0;; id++) {
 		bool found = false;
@@ -115,62 +118,82 @@ int NextDeviceIndex(const MidiProduct& device) {
 }
 
 void AddDevice(const MidiDevice& device) {
-	DPrintf("Device Add: %s\n", device.toString().c_str());
-	auto map = deviceMap.find(device);
-	if (map == deviceMap.end()) {
-		deviceMap.insert(std::make_pair(device, std::set<int>()));
-		map = deviceMap.find(device);
+	DPrintf("Device Add: %s (HMIDIIN: %08x)\n", device.toString().c_str(), device.mHMidi);
+	auto map = productMap.find(device);
+	if (map == productMap.end()) {
+		productMap.insert(std::make_pair(device, std::set<int>()));
+		map = productMap.find(device);
 	}
 	map->second.insert(device.mIndex);
-	deviceList.push_back(device);
+	deviceSet.insert(device);
+}
+
+void RemoveDevice(const MidiDevice& device) {
+	DPrintf("Device Remove: %s\n", device.toString().c_str());
+	productMap.find(device)->second.erase(device.mIndex);
+	deviceSet.erase(device);
 }
 
 void RemoveProduct(const MidiProduct& product) {
-	DPrintf("Device Remove: %s\n", product.toString().c_str());
-	auto map = deviceMap.find(product);
-	if (map == deviceMap.end()) {
-		DPrintf("**** should not happen: device not found in map ****");
+	DPrintf("Product Remove: %s\n", product.toString().c_str());
+	auto map = productMap.find(product);
+	auto index = map->second.rbegin();
+	if (index == map->second.rend())
 		return;
-	}
-	auto i = map->second.rbegin();
-	int index = *i;
-	map->second.erase(*i);
-	deviceList.remove(MidiDevice(map->first, index));
+	RemoveDevice(MidiDevice(product, *index));
+}
+
+void CALLBACK MidiInCallback(HMIDIIN hMidiIn, UINT uMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2) {
+	if (uMsg != MIM_CLOSE)
+		return;
+	auto device = handleMap.find(hMidiIn);
+	if (device == handleMap.end())
+		return;
+	RemoveDevice(device->second);
+	handleMap.erase(hMidiIn);
+	// midiInClose should be called later (Note: forbidden to call from here)
+	closingHandleSet.insert(hMidiIn);
 }
 
 void UpdateDeviceList() {
 	DPrintf("UpdateDeviceList\n");
 
 	// Add new devices.
-	std::map<MidiProduct, int> newDeviceMap;
+	std::map<MidiProduct, int> newProductMap;
 	UINT uNumDevs = midiInGetNumDevs();
 	for (UINT i = 0; i < uNumDevs; ++i) {
 		MIDIINCAPS2 caps;
 		midiInGetDevCaps(i, reinterpret_cast<LPMIDIINCAPS>(&caps), sizeof(caps));
-		MidiProduct device(caps);
-		auto newEntry = newDeviceMap.find(device);
+		MidiProduct product(caps);
+		auto newEntry = newProductMap.find(product);
 		int newDeviceCount = 1;
-		if (newEntry == newDeviceMap.end()) {
-			newDeviceMap.insert(std::make_pair(device, 1));
+		if (newEntry == newProductMap.end()) {
+			newProductMap.insert(std::make_pair(product, 1));
 		} else {
 			newEntry->second++;
 			newDeviceCount = newEntry->second;
 		}
-		auto entry = deviceMap.find(device);
+		auto entry = productMap.find(product);
 		int deviceCount = 0;
-		if (entry != deviceMap.end())
+		if (entry != productMap.end())
 			deviceCount = entry->second.size();
-		if (newDeviceCount > deviceCount)
-			AddDevice(MidiDevice(caps, NextDeviceIndex(device)));
+		if (newDeviceCount > deviceCount) {
+			MidiDevice device(caps, NextDeviceIndex(product));
+#ifndef NOPEN  // If you do not want to open the device here, define NOPEN.
+			midiInOpen(&device.mHMidi, i, reinterpret_cast<DWORD_PTR>(&MidiInCallback), NULL, CALLBACK_FUNCTION);
+			handleMap.insert(std::make_pair(device.mHMidi, device));
+#endif
+			AddDevice(device);
+		}
 	}
 
 	// Remove disconnected devices that are not opened.
 	// If we have multiple sam edevices, we can not figure out which device is disconnected.
 	std::vector<MidiProduct> removeList;
-	for (auto device : deviceMap) {
+	for (auto device : productMap) {
 		int disconnectedDevices = device.second.size();
-		auto newMap = newDeviceMap.find(device.first);
-		if (newMap != newDeviceMap.end())
+		auto newMap = newProductMap.find(device.first);
+		if (newMap != newProductMap.end())
 			disconnectedDevices -= newMap->second;
 		for (int i = 0; i < disconnectedDevices; ++i)
 			removeList.push_back(device.first);
@@ -179,11 +202,15 @@ void UpdateDeviceList() {
 	for (auto device : removeList)
 		RemoveProduct(device);
 
+	for (auto handle : closingHandleSet)
+		midiInClose(handle);
+	closingHandleSet.clear();
+
 	DPrintf("=== new device list ===\n");
-	for (auto device : deviceList)
+	for (auto device : deviceSet)
 		DPrintf("%s\n", device.toString().c_str());
 	DPrintf("=== new device map ===\n");
-	for (auto product : deviceMap)
+	for (auto product : productMap)
 		DPrintf("%s: x%d\n", product.first.getDisplayName().c_str(), product.second.size());
 }
 
@@ -204,8 +231,8 @@ LRESULT CALLBACK DeviceNotifyWindowProc(HWND hWnd, UINT message, WPARAM wParam, 
 	return 0;
 }
 
+// Experimental code for http://crbug.com/472980
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow) {
-	// Use 
 	WNDCLASSEX wcex;
 	ZeroMemory(&wcex, sizeof(wcex));
 	wcex.cbSize = sizeof(wcex);
@@ -214,6 +241,13 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstanc
 	wcex.lpszClassName = _T("DeviceNotifyWindowClass");
 	RegisterClassEx(&wcex);
 	HWND hWnd = CreateWindow(wcex.lpszClassName, _T("MIDIConectpTest - DeviceNotify"), 0, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+#if 0
+	DEV_BROADCAST_DEVICEINTERFACE filter;
+	filter.dbcc_size = sizeof(filter);
+	filter.dbcc_devicetype = DBT_DEVTYP_DEVICEINTERFACE;
+	RegisterDeviceNotification(hWnd, &filter, DEVICE_NOTIFY_WINDOW_HANDLE | DEVICE_NOTIFY_ALL_INTERFACE_CLASSES);
+#endif
 
 	UpdateDeviceList();
 
